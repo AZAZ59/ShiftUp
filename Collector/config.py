07be7891e0dac0ef152ee5eb6f6244a4519166f0
Stepanov Sergey <step-srg@yandex.ru>
2017-06-05 19:55:35 +0400

    init1

--- Collector/config.py
+++ Collector/config.py
@@ -3,7 +3,7 @@
 
 THREAD_COUNT_DEFAULT = 45
 
-DEFAULT_TOKEN = '5eed487cbd3f61ac8f53581a06906edf3fb6019cb65051b69977abac6ce7672c446a688754c34fa3b6a18'
+DEFAULT_TOKEN = '**************************************'
 MEMBERS_FIELDS = 'sex, bdate, city, country, online, online_mobile, domain, has_mobile, contacts, connections,' \
                  ' site, education, universities, schools, status, last_seen, relation, relatives'
 GROUPS_FIELDS = 'city, country, place, description, wiki_page, members_count, counters, start_date, finish_date, ' \
